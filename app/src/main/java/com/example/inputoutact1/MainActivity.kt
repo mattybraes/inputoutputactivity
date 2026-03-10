e9bc130d0c5b9f0a8f2d0a4dcdf276748469ea3b
mattybraes <matthewmeiklebraes@gmail.com>
2026-03-10 16:32:25 +0200

    comment i did working with variables

--- app/src/main/java/com/example/inputoutact1/MainActivity.kt
+++ app/src/main/java/com/example/inputoutact1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.inputoutact1
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // get the button using the id we set on the user interface
+        val clickMeButton = findViewById<Button>(R.id.clickMeButton)
+
+        // add code to the code to the button that happens when it is clicked
+        clickMeButton?.setOnClickListener {
+            Toast.makeText(this@MainActivity,
+                   "Button clicked", Toast.LENGTH_LONG).show()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
