57cf5174e229349c461f58aeede0c0d335c61c71
mattybraes <matthewmeiklebraes@gmail.com>
2026-03-18 09:20:31 +0200

    added code to switch

--- app/src/main/java/com/example/inputoutact1/MainActivity.kt
+++ app/src/main/java/com/example/inputoutact1/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.inputoutact1
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Switch
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -12,28 +14,60 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.inputoutact1.R.id.nameTxt
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val clickmebutton =findViewById<Button>(R.id.clickmebutton)
-        val welcomeTxt = findViewById<TextView>(R.id.displayTxt)
-        val nameTxtField = findViewById<EditText>(R.id.nameTxt)
-
+        val clickmebutton = findViewById<Button>(R.id.clickmebutton)
+        val welcomeTxt = findViewById<TextView>(R.id.nameTxt)
+        val nameTxtField = findViewById<EditText>(R.id.displayTxt)
 
         // get the button using the id we set on the user interface
         // val clickMeButton = findViewById<Button>(R.id.clickmebutton)
 
         // add code to the code to the button that happens when it is clicked
         clickmebutton?.setOnClickListener {
-            Toast.makeText(this@MainActivity,
-                   "Button clicked", Toast.LENGTH_LONG).show()
+            Toast.makeText(
+                this@MainActivity,
+                "Button clicked", Toast.LENGTH_LONG
+            ).show()
+            welcomeTxt.text = "Welcome ${nameTxtField.text}"
+            //welcomeTxt.text = "Welcome" * nameTxtField.text
+
+            //switching between 2 languages. zulu and english
+            val zuluSwitch = findViewById<Switch>(R.id.zuluSwitch)
+            //when button is clicked the code is activated
+            var greeting: String
+
+            var zulu: Boolean = zuluSwitch.isChecked
+            if (zulu)
+                greeting = "Sawubona, ${nameTxtField}!"
+            else {
+                if (nameTxtField.text.toString() == "sam" ||
+                    nameTxtField.text.toString() == "samantha"
+                ) {
+                    greeting = "yo, ${nameTxtField}!"
+
+                    greeting = "Greetings, ${nameTxtField}!"
+                } else {
+                    greeting = "greetings, ${nameTxtField.text}!"
+                }
+
+
+
+                welcomeTxt.text = greeting
+
+
+            }
+
+
+
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
         }
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
-}
+    }}
